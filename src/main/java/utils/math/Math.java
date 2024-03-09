3aa969ca6d1790e59856b651b64fac128404c5bb
Mahmoud Ahmed Ramadan <mahmoudahmed.web@gmail.com>
2024-03-09 17:34:10 +0100

    build: add Lombok support to the project in pom.xml

--- src/main/java/utils/math/Math.java
+++ src/main/java/utils/math/Math.java
@@ -1,7 +1,15 @@
 package utils.math;
 
+@
 public class Math {
+    // Finding maximum of integers
 
+    public static int findMax(int[] numbers) {
+        if (numbers == null || numbers.length == 0) {
+            throw new IllegalArgumentException("Array is empty or null");
+        }
+
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello world!");

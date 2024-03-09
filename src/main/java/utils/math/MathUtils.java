05f838c3f093c1727c695485df7584073e6b740e
Mahmoud Ahmed Ramadan <mahmoudahmed.web@gmail.com>
2024-03-09 19:59:24 +0100

    build: added checkstyle plugin to pom.xml file

--- src/main/java/utils/math/MathUtils.java
+++ src/main/java/utils/math/MathUtils.java
@@ -160,21 +160,5 @@ public class MathUtils {
             throw new IllegalArgumentException("Array is empty or null");
         }
     }
-    public static void main(String[] args) {
-        int[] numbers = new int[5];
-
-        numbers[0] = 1;
-        numbers[1] = 2;
-        numbers[2] = 3;
-        numbers[3] = 4;
-        numbers[4] = 5;
-
-        System.out.println(MathUtils.findMax(numbers));
-        System.out.println(MathUtils.findMin(numbers));
-        System.out.println(MathUtils.sum(numbers));
-        System.out.println(MathUtils.calculateStandardDeviation(numbers));
-
-    }
-
 
 }

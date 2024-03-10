6f8de0f7430ac64d315c0be5b5b2db879c866e19
Mahmoud Ahmed Ramadan <mahmoudahmed.web@gmail.com>
2024-03-10 08:48:38 +0100

    test: added test class for an ArrayList

---
+++ src/test/java/ArrayListTest.java
@@ -0,0 +1,25 @@
+import utils.datastructure.ArrayList;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        // Add elements to the ArrayList
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        // Print the size of the ArrayList
+        System.out.println("Size: " + list.size()); // Should print 3
+
+        // Print the elements of the ArrayList
+        System.out.print("Elements: ");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println(); // Should print "1 2 3"
+
+        // Access an element by index
+        System.out.println("Element at index 1: " + list.get(1)); // Should print 2
+    }
+}

8bfa09f3187bde086ff561687010035735a9aade
Mahmoud Ahmed Ramadan <mahmoudahmed.web@gmail.com>
2024-03-10 07:50:09 +0100

    added a documentation for StackLinkedList generic class

--- src/main/java/utils/datastructure/StackLinkedList.java
+++ src/main/java/utils/datastructure/StackLinkedList.java
@@ -2,15 +2,28 @@ package utils.datastructure;
 
 import java.util.EmptyStackException;
 
+/**
+ * A simple implementation of a stack using a linked list.
+ *
+ * @param <T> the type of elements stored in the stack
+ */
 public class StackLinkedList<T> {
-    private Node<T> top;
-    private int size;
+    private Node<T> top; // Top of the stack
+    private int size;    // Size of the stack
 
+    /**
+     * Constructs an empty stack.
+     */
     public StackLinkedList() {
         top = null;
         size = 0;
     }
 
+    /**
+     * Pushes an element onto the top of this stack.
+     *
+     * @param data the element to push onto the stack
+     */
     public void push(T data) {
         Node<T> newNode = new Node<>(data);
         newNode.next = top;
@@ -18,6 +31,12 @@ public class StackLinkedList<T> {
         size++;
     }
 
+    /**
+     * Removes the element at the top of this stack and returns it.
+     *
+     * @return the element removed from the top of the stack
+     * @throws EmptyStackException if the stack is empty
+     */
     public T pop() {
         if (isEmpty()) {
             throw new EmptyStackException();
@@ -28,6 +47,12 @@ public class StackLinkedList<T> {
         return data;
     }
 
+    /**
+     * Returns the element at the top of this stack without removing it.
+     *
+     * @return the element at the top of the stack
+     * @throws EmptyStackException if the stack is empty
+     */
     public T peek() {
         if (isEmpty()) {
             throw new EmptyStackException();
@@ -35,18 +60,36 @@ public class StackLinkedList<T> {
         return top.data;
     }
 
+    /**
+     * Checks if the stack is empty.
+     *
+     * @return true if the stack is empty, false otherwise
+     */
     public boolean isEmpty() {
         return top == null;
     }
 
+    /**
+     * Returns the number of elements in the stack.
+     *
+     * @return the number of elements in the stack
+     */
     public int size() {
         return size;
     }
 
+    /**
+     * Represents a node in the linked list.
+     */
     private static class Node<T> {
-        private T data;
-        private Node<T> next;
+        private T data;       // Data stored in the node
+        private Node<T> next; // Reference to the next node
 
+        /**
+         * Constructs a node with the given data.
+         *
+         * @param data the data to store in the node
+         */
         public Node(T data) {
             this.data = data;
             this.next = null;

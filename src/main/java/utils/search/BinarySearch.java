fca6ecae3b30a7d346a5e66de3dd30ca3a528ea9
Mahmoud Ahmed Ramadan <mahmoudahmed.web@gmail.com>
2024-03-10 09:08:56 +0100

    implemented BinarySearch algorithm and its test class

--- src/main/java/utils/search/BinarySearch.java
+++ src/main/java/utils/search/BinarySearch.java
@@ -1,4 +1,29 @@
 package utils.search;
 
-public class BinarySearch {
+public class BinarySearch<T extends Comparable<T>> {
+    /**
+     * Searches for a target value within a sorted array using the binary search algorithm.
+     * @param array The sorted array to search in.
+     * @param target The value to search for.
+     * @return The index of the target value if found, or -1 if not found.
+     */
+    public int search(T[] array, T target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            int comparisonResult = target.compareTo(array[mid]);
+            if (comparisonResult == 0) {
+                return mid; // Target found
+            } else if (comparisonResult > 0) {
+                left = mid + 1; // Search the right half
+            } else {
+                right = mid - 1; // Search the left half
+            }
+        }
+
+        return -1; // Target not found
+    }
 }
---
+++ src/test/java/BinarySearchTest.java
@@ -0,0 +1,19 @@
+import utils.search.BinarySearch;
+
+public class BinarySearchTest {
+    public static void main(String[] args) {
+        // Test case with integers
+        Integer[] intArray = {1, 3, 5, 7, 9, 11, 13, 15};
+        BinarySearch<Integer> integerBinarySearch = new BinarySearch<>();
+        int targetInt = 7;
+        int indexInt = integerBinarySearch.search(intArray, targetInt);
+        System.out.println("Index of " + targetInt + " in the integer array: " + indexInt);
+
+        // Test case with strings
+        String[] stringArray = {"apple", "banana", "cherry", "grape", "orange", "strawberry"};
+        BinarySearch<String> stringBinarySearch = new BinarySearch<>();
+        String targetString = "orange";
+        int indexString = stringBinarySearch.search(stringArray, targetString);
+        System.out.println("Index of \"" + targetString + "\" in the string array: " + indexString);
+    }
+}
